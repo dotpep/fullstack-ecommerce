2a1f4fe87aa0f0bc0e53ee0fad31e2fdb61ddd1e
dotpep <pepelnine@gmail.com>
2024-02-05 23:18:13 +0600

    Implement models for payment app ShippingAddress, Order and OrderItem with register it in admin panel

--- core/payment/models.py
+++ core/payment/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from shop.models import Product
+
+User = get_user_model()
+
+
+class ShippingAddress(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254)
+
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    zip_code = models.CharField(max_length=100, blank=True, null=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"ShippingAddress: {self.id}"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.CASCADE, blank=True, null=True)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"Order: {self.id}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"OrderItem: {self.id}"
