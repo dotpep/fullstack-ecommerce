bb558b921cd9d730a773f627d361be55623e1e91
dotpep <pepelnine@gmail.com>
2024-02-07 01:00:33 +0600

    Connect Stripe api payment method to cart checkout

--- core/payment/views.py
+++ core/payment/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.urls import reverse
+from decimal import Decimal
 
 from cart.cart import Cart
 
 from .forms import ShippingAddressForm
 from .models import Order, OrderItem, ShippingAddress
+
+from django.conf import settings
+
+# stripe
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+stripe.api_version = settings.STRIPE_API_VERSION
+
 
 # type hinting
 from django.http import HttpRequest
@@ -29,7 +40,6 @@ def shipping_view(request: HttpRequest):
             shipping_address = form.save(commit=False)
             shipping_address.user = request.user
             shipping_address.save()
-            #return redirect('account:dashboard')
             return redirect('payment:checkout')
     context = {'form': form}
     return render(request, 'shipping/shipping.html', context)
@@ -45,7 +55,9 @@ def checkout_view(request: HttpRequest):
     return render(request, 'payment/checkout.html')
 
 def complete_order(request: HttpRequest):
-    if request.POST.get('action') == "payment":
+    if request.method == 'POST':
+        payment_type = request.POST.get('stripe-payment', 'yookassa-payment')
+
         name = request.POST.get('name')
         email = request.POST.get('email')
         street_address = request.POST.get('street_address')
@@ -57,49 +69,70 @@ def complete_order(request: HttpRequest):
         cart = Cart(request)
         total_price = cart.get_total_price()
         
-        shipping_address, _ = ShippingAddress.objects.get_or_create(
-            user=request.user,
-            defaults={
-                'full_name': name,
-                'email': email,
-                'street_address': street_address,
-                'apartment_address': apartment_address,
-                'country': country,
-                'city': city,
-                'zip_code': zip_code,
-            }
-        )
-        
-        if request.user.is_authenticated:
-            order = Order.objects.create(
-                user=request.user,
-                shipping_address=shipping_address,
-                total_price=total_price,
-            )
-            
-            for item in cart:
-                OrderItem.objects.create(
-                    order=order,
-                    product=item['product'],
-                    price=item['price'],
-                    quantity=item['qty'],
+        # FIXME: proccessed only one product instead 2... choosen in cart in payment method with stripe 
+        match payment_type:
+            case 'stripe-payment':
+                shipping_address, _ = ShippingAddress.objects.get_or_create(
                     user=request.user,
-                )
-        else:
-            order = Order.objects.create(
-                shipping_address=shipping_address,
-                total_price=total_price,
-            )
-            
-            for item in cart:
-                OrderItem.objects.create(
-                    order=order,
-                    product=item['product'],
-                    price=item['price'],
-                    quantity=item['qty'],
+                    defaults={
+                        'full_name': name,
+                        'email': email,
+                        'street_address': street_address,
+                        'apartment_address': apartment_address,
+                        'country': country,
+                        'city': city,
+                        'zip_code': zip_code,
+                    }
                 )
 
-        return JsonResponse({'success': True})
+                session_data = {
+                    'mode': 'payment',
+                    'success_url': request.build_absolute_uri(reverse('payment:payment-success')),
+                    'cancel_url': request.build_absolute_uri(reverse('payment:payment-fail')),
+                    'line_items': [],
+                }
+                
+                if request.user.is_authenticated:
+                    order = Order.objects.create(
+                        user=request.user,
+                        shipping_address=shipping_address,
+                        total_price=total_price,
+                    )
+                    
+                    for item in cart:
+                        OrderItem.objects.create(
+                            order=order,
+                            product=item['product'],
+                            price=item['price'],
+                            quantity=item['qty'],
+                            user=request.user,
+                        )
+                        session_data['line_items'].append({
+                            'price_data': {
+                                'unit_amount': int(item['price'] * Decimal(100)),
+                                'currency': 'usd',
+                                'product_data': {
+                                    'name': item['product'],
+                                },
+                            },
+                            'quantity': item['qty'],
+                        })
+                        
+                        session = stripe.checkout.Session.create(**session_data)
+                        return redirect(session.url, code=303)
+                else:
+                    order = Order.objects.create(
+                        shipping_address=shipping_address,
+                        total_price=total_price,
+                    )
+                    
+                    for item in cart:
+                        OrderItem.objects.create(
+                            order=order,
+                            product=item['product'],
+                            price=item['price'],
+                            quantity=item['qty'],
+                        )
 
 
 def payment_success(request: HttpRequest):
